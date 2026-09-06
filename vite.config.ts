6a958b76eaf4d05f7c4aa83fed9bd823a59b3874
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-06 20:25:58 +0700

    fix: support github pages and custom domain paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
   worker: { format: 'es' },
   build: { target: 'es2022' },

9dc15361cb7849f739ab16ec53bcb992676ef971
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-06 19:36:18 +0700

    build: configure vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/compress-image/',
   plugins: [react()],
   worker: { format: 'es' },
   build: { target: 'es2022' },

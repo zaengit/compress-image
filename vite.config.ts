a9f64fd8ae7e5e5d20ce7c85a1e96a446c6f4ac4
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-06 20:25:17 +0700

    config: serve app from custom domain root

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/compress-image/',
+  base: '/',
   plugins: [react()],
   worker: { format: 'es' },
   build: { target: 'es2022' },
